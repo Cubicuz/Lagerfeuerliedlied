db03ed6503657c3bf0d5dc2040d1c83ed0ff1d87
Cubicuz <michaelschmelcher@t-online.de>
2014-11-04 22:46:11 +0100

    favoriten css js

--- Meilenstein 3/js/favoriten.js
+++ Meilenstein 3/js/favoriten.js
@@ -1,21 +1,23 @@
-var musikClicked=true;
-
 function onload(){
     musik();
 }
 
 function musik(){
-    musikClicked=true;
     var o1 = document.getElementById("musikButton");
-    o1.style.backgroundColor = "rgb(165,169,230)";
+    o1.className = "selected";
+    
     var o2 = document.getElementById("filmButton");
-    o2.style.backgroundColor = "rgb(26,32,94)";
+    o2.className = "button";
 }
 
 function film(){
-    musikClicked=false;
     var o1 = document.getElementById("filmButton");
-    o1.style.backgroundColor = "rgb(165,169,230)";
+    o1.className = "selected"
     var o2 = document.getElementById("musikButton");
-    o2.style.backgroundColor = "rgb(26,32,94)";
+    o2.className = "button";
+}
+
+function load (){
+    var table = "";
+    
 }
