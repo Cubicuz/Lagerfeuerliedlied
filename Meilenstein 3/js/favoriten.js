969e4fb662c384f77daaab8b5282c7c795d42b23
Kroemelmonster <PHouligans@web.de>
2014-11-07 12:27:41 +0100

    Changed: favoriten vom style angepasst

--- Meilenstein 3/js/favoriten.js
+++ Meilenstein 3/js/favoriten.js
@@ -22,20 +22,29 @@ function film(){
 
 function loadFilm (){
     var table = "";
-    table += '<table border="1">';
-    table += '<tr> <th>Filmtitel</th> <th>Regie</th> <th>Drehbuch</th> <th>Erscheinungsjahr</th> <th>Genre</th> </tr>';   
+    table += '<table>';
+    table += '<tr> <td>Filmtitel</td> <td>Regie</td> <td>Drehbuch</td> <td>Erscheinungsjahr</td> <td>Genre</td> </tr>';   
     for (var i=0; i<filmfav.length;i++){
-        table += '<tr> <td>' + filmfav[i].Filmtitel + '</td> <td>' + filmfav[i].Regie + '</td> <td>' + filmfav[i].Drehbuch + '</td> <td>' + filmfav[i].Erscheinungsjahr + '</td> <td>' + filmfav[i].Genre + '</td></tr>';
+        table += '<tr> <td>' + filmfav[i].Filmtitel 
+		+ '</td> <td>' + filmfav[i].Regie 
+		+ '</td> <td>' + filmfav[i].Drehbuch 
+		+ '</td> <td>' + filmfav[i].Erscheinungsjahr 
+		+ '</td> <td>' + filmfav[i].Genre 
+		+ '</td></tr>';
     }
     document.getElementById("tabelle").innerHTML = table;
 }
 
 function loadMusik (){
     var table = "";
-    table += '<table border="1">';
-    table += '<tr> <th>Interpreter</th> <th>Albumtitel</th> <th>Erscheinungsjahr</th> <th>Genre</th> </tr>';   
+    table += '<table>';
+    table += '<tr> <td>Interpreter</td> <td>Albumtitel</td> <td>Erscheinungsjahr</td> <td>Genre</td> </tr>';   
     for (var i=0; i<musicfav.length;i++){
-        table += '<tr> <td>' + musicfav[i].Interpreter + '</td> <td>' + musicfav[i].Albumtitel + '</td> <td>' + musicfav[i].Erscheinungsjahr + '</td> <td>' + musicfav[i].Genre + '</td></tr>';
+        table += '<tr> <td>' + musicfav[i].Interpreter 
+		+ '</td> <td>' + musicfav[i].Albumtitel 
+		+ '</td> <td>' + musicfav[i].Erscheinungsjahr 
+		+ '</td> <td>' + musicfav[i].Genre 
+		+ '</td></tr>';
     }
     document.getElementById("tabelle").innerHTML = table;
   
