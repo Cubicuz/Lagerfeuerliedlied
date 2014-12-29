3eff5fed5fec8e2719c752a72a69e3b4b8f7888a
Cubicuz <michaelschmelcher@t-online.de>
2014-12-29 18:01:39 +0100

    Meilenstein 6 SQL Abfragen funktionieren nicht
    Keine Ahnung warum ich kein result aus den SQL abfragen bekomme, wenn
    ich die gleichen abfragen in mysql Worbench mache funktioniert alles
    einwandfrei

--- Meilenstein 6/php/getFavorites.php
+++ Meilenstein 6/php/getFavorites.php
@@ -1,35 +1,41 @@
 <?php
-$mysqli = mysqli_connect("localhost","myMedia","megapasswort","media");
+if($mysqli = mysqli_connect("localhost","myMedia","megapasswort","media")){
+    echo("Verbindung klappt");
+} else {
+    echo("keine Verbindung");
+}
 $music = true;
-foreach($_GET as $key => $value) {
-	$pos = strpos($key,"film");
-	if ($pos !== false) {
-		$music = false;
-		break;
-	}
+if($_GET['filename']=="film_json"){
+    $music = false;
 }
 if($music){
     $sql = "SELECT interpreter,albumtitel,erscheinungsjahr,genre FROM music";
-    $result = $mysqli->query(sql);
-    $data="[";
-    while($row=mysqli_fetch_row($result)){
-        $data .= "{'Interpreter':'$row[0]','Albumtitel':'$row[1]','Erscheinungsjahr':'$row[2]','Genre':'$row[3]'},";
+    if ($result = $mysqli->query($sql)){
+        $data="[";
+        while($row=$result->fetch_assoc()){
+            $data .= "{'Interpreter':'$row[0]','Albumtitel':'$row[1]','Erscheinungsjahr':'$row[2]','Genre':'$row[3]'},";
+        }
+        //sonst ist ein Komma zu viel
+        $data = substr($data,0,-1);
+        $data .="]";
+        echo ($data);
+    } else {
+        echo ("fehler beim Musik result");
     }
-    //sonst ist ein Komma zu viel
-    $data = substr($data,0,-1);
-    $data .="]";
-    echo ($data);
 } else {
-    $sql = "SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film";
-    $result = $mysqli->query(sql);
-    $data="[";
-    while($row=mysqli_fetch_row($result)){
-        $data .= "{'Filmtitel':'$row[0]','Regie':'$row[1]','Drehbuch':'$row[2]','Erscheinungsjahr':'$row[3]','Genre':'$row[4]'},";
+    $sqlSelect = "SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film";
+    if ($result = $mysqli->query($sqlSelect)) {
+        $data="[";
+        while($row=$result->fetch_assoc()){
+            $data .= "{'Filmtitel':'$row[0]','Regie':'$row[1]','Drehbuch':'$row[2]','Erscheinungsjahr':'$row[3]','Genre':'$row[4]'},";
+        }
+        //sonst ist ein Komma zu viel
+        $data = substr($data,0,-1);
+        $data .="]";
+        echo($data);
+    } else {
+        echo ("fehler beim Film result");
     }
-    //sonst ist ein Komma zu viel
-    $data = substr($data,0,-1);
-    $data .="]";
-    echo($data);
 }
 mysqli_close($mysqli);
 ?>
--- Meilenstein 6/php/immdb.php
+++ Meilenstein 6/php/immdb.php
@@ -1,13 +1,14 @@
 <?php
-//CREATE USER 'myMedia'@'localhost' IDENTIFIED BY 'megapasswort':
+//CREATE USER 'myMedia'@'localhost' IDENTIFIED BY 'megapasswort';
 //CREATE DATABASE media;
 //GRANT CREATE ON media.* TO 'myMedia'@'localhost';
 //GRANT DROP ON media.* TO 'myMedia'@'localhost';
 //GRANT DELETE ON media.* TO 'myMedia'@'localhost';
 //GRANT INSERT ON media.* TO 'myMedia'@'localhost';
 //GRANT UPDATE ON media.* TO 'myMedia'@'localhost';
-//CREATE TABLE music(mid INTEGER AUTO_INCREMENT PRIMARY KEY,interpreter char(50),albumtitel char(50),erscheinungsjahr INTEGER,songs varchar(500),genre char(50),favorit BOOlEAN)
-//CREATE TABLE film(fid INTEGER AUTO_INCREMENT PRIMARY KEY,filmtitel char(50),regie char(50),drehbuch char(50),erscheinungsjahr integer,schauspieler varchar(500),genre char(50),favorit BOOLEAN)
+//USE media;
+//CREATE TABLE music(mid INTEGER AUTO_INCREMENT PRIMARY KEY,interpreter char(50),albumtitel char(50),erscheinungsjahr INTEGER,songs varchar(500),genre char(50),favorit BOOlEAN);
+//CREATE TABLE film(fid INTEGER AUTO_INCREMENT PRIMARY KEY,filmtitel char(50),regie char(50),drehbuch char(50),erscheinungsjahr integer,schauspieler varchar(500),genre char(50),favorit BOOLEAN);
 $mysqli = mysqli_connect("localhost","myMedia","megapasswort","media");
 $music = true;
 foreach($_GET as $key => $value) {
@@ -18,34 +19,34 @@ foreach($_GET as $key => $value) {
 	}
 }
 if ($music){
-    $interpreter = $_POST['interpreter'];
-    $albumtitel = $_POST['albumtitel'];
-    $jahr = $_POST['musicerscheinungsjahr'];
-    $songs = $_POST['songs'];
-    $genre = $_POST['musicgenre'];
-    $favorit = isset($_POST['musicfavorit']);
+    $interpreter = $_GET['interpreter'];
+    $albumtitel = $_GET['albumtitel'];
+    $jahr = $_GET['musicerscheinungsjahr'];
+    $songs = $_GET['songs'];
+    $genre = $_GET['musicgenre'];
+    $favorit = $_GET['musicfavorit']==1;
     
     //falls das Lied schon eingetragen ist wird kein neuer Eintrag erstellt
-    $sql="SELECT interpreter,albumtitel,erscheinungsjahr,genre FROM music WHERE  interpreter=$interpreter AND albumtitel=$albumtitel AND erscheinungsjahr=$jahr";
+    $sql="SELECT interpreter,albumtitel,erscheinungsjahr,genre FROM music WHERE  interpreter='$interpreter' AND albumtitel='$albumtitel' AND erscheinungsjahr='$jahr'";
     $result = $mysqli->query($sql);
     if ($result->num_rows < 1) {
-        $sqlInsert = "INSERT INTO music (interpreter,albumtitel,erscheinungsjahr,songs,genre,favorit) VALUES ($interpreter,$albumtitel,$jahr,$songs,$genre,$favorit)";
+        $sqlInsert = "INSERT INTO music (interpreter,albumtitel,erscheinungsjahr,songs,genre,favorit) VALUES ('$interpreter','$albumtitel','$jahr','$songs','$genre','$favorit')";
         $result = $mysqli->query($sqlInsert);
     }
 } else {
-    $filmtitel = $_POST['filmtitel'];
-    $regie = $_POST['regie'];
-    $drehbuch = $_POST['drehbuch'];
-    $erscheinungsjahr = $_POST['erscheinungsjahr'];
-    $schauspieler = $_POST['schauspieler'];
-    $filmgenre = $_POST['filmgenre'];
-    $filmfavorit = isset($_POST(['filmfavorit']));
+    $filmtitel = $_GET['filmtitel'];
+    $regie = $_GET['regie'];
+    $drehbuch = $_GET['drehbuch'];
+    $erscheinungsjahr = $_GET['erscheinungsjahr'];
+    $schauspieler = $_GET['schauspieler'];
+    $filmgenre = $_GET['filmgenre'];
+    $filmfavorit = $_GET['filmfavorit']==1;
 
     //falls der Film schon eingetragen ist wird kein neuer Eintrag erstellt
-    $sql="SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film WHERE filmtitel=$filmtitel AND regie=$regie AND drehbuch=$drehbuch AND erscheinungsjahr=$erscheinungsjahr AND genre=$filmgenre";
+    $sql="SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film WHERE filmtitel='$filmtitel' AND regie='$regie' AND drehbuch='$drehbuch' AND erscheinungsjahr='$erscheinungsjahr' AND genre='$filmgenre'";
     $result = $mysqli->query($sql);
     if ($result->num_rows < 1) {
-        $sqlInsert="INSERT INTO film (filmtitel,regie,drehbuch,erscheinungsjahr,schauspieler,genre,favorit) VALUES ($filmtitel,$regie,$drehbuch,$erscheinungsjahr,$schauspiler,$filmgenre,$filmfavorit)";
+        $sqlInsert="INSERT INTO film (filmtitel,regie,drehbuch,erscheinungsjahr,schauspieler,genre,favorit) VALUES ('$filmtitel','$regie','$drehbuch','$erscheinungsjahr','$schauspiler','$filmgenre','$filmfavorit')";
         $result = $mysqli->query($sqlInsert);
     }
 }
