72e1227254de7bfc1c493fadde0cead32d1e52af
Cubicuz <michaelschmelcher@t-online.de>
2014-12-29 16:07:30 +0100

    Meilenstein 6
    in immdb.php steht in den Kommentaren wie man die mysql Datenbank
    einrichten muss

--- Meilenstein 6/php/getFavorites.php
+++ Meilenstein 6/php/getFavorites.php
@@ -1,8 +1,35 @@
 <?php
-	
-	$filename = "../JSON/".$_GET['filename'].".js";
-	$handle = fopen($filename, 'r') or die('Cannot open file:  '.$filename);
-	$data = fread($handle, filesize($filename));
-	echo ($data);
-	fclose($handle);
+$mysqli = mysqli_connect("localhost","myMedia","megapasswort","media");
+$music = true;
+foreach($_GET as $key => $value) {
+	$pos = strpos($key,"film");
+	if ($pos !== false) {
+		$music = false;
+		break;
+	}
+}
+if($music){
+    $sql = "SELECT interpreter,albumtitel,erscheinungsjahr,genre FROM music";
+    $result = $mysqli->query(sql);
+    $data="[";
+    while($row=mysqli_fetch_row($result)){
+        $data .= "{'Interpreter':'$row[0]','Albumtitel':'$row[1]','Erscheinungsjahr':'$row[2]','Genre':'$row[3]'},";
+    }
+    //sonst ist ein Komma zu viel
+    $data = substr($data,0,-1);
+    $data .="]";
+    echo ($data);
+} else {
+    $sql = "SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film";
+    $result = $mysqli->query(sql);
+    $data="[";
+    while($row=mysqli_fetch_row($result)){
+        $data .= "{'Filmtitel':'$row[0]','Regie':'$row[1]','Drehbuch':'$row[2]','Erscheinungsjahr':'$row[3]','Genre':'$row[4]'},";
+    }
+    //sonst ist ein Komma zu viel
+    $data = substr($data,0,-1);
+    $data .="]";
+    echo($data);
+}
+mysqli_close($mysqli);
 ?>
--- Meilenstein 6/php/immdb.php
+++ Meilenstein 6/php/immdb.php
@@ -6,9 +6,8 @@
 //GRANT DELETE ON media.* TO 'myMedia'@'localhost';
 //GRANT INSERT ON media.* TO 'myMedia'@'localhost';
 //GRANT UPDATE ON media.* TO 'myMedia'@'localhost';
-//CREATE TABLE m_interpreter(iid INTEGER NOT NULL AUTO_INCREMENT,name CHAR(50),PRIMARY KEY(iid));
-//CREATE TABLE m_albumtitel(aid INTEGER NOT NULL AUTO_INCREMENT,titel CHAR(50),songs VARCHAR(500),jahr INTEGER,favorit BOOL,PRIMARY KEY(aid));
-//CREATE TABLE m_interpreter_albumtitel(iid INTEGER NOT NULL,aid INTEGER NOT NULL,FOREIGN KEY(iid) REFERENCES m_interpreter(iid),FOREIGN KEY(aid) REFERENCES m_albumtitel(aid));
+//CREATE TABLE music(mid INTEGER AUTO_INCREMENT PRIMARY KEY,interpreter char(50),albumtitel char(50),erscheinungsjahr INTEGER,songs varchar(500),genre char(50),favorit BOOlEAN)
+//CREATE TABLE film(fid INTEGER AUTO_INCREMENT PRIMARY KEY,filmtitel char(50),regie char(50),drehbuch char(50),erscheinungsjahr integer,schauspieler varchar(500),genre char(50),favorit BOOLEAN)
 $mysqli = mysqli_connect("localhost","myMedia","megapasswort","media");
 $music = true;
 foreach($_GET as $key => $value) {
@@ -26,43 +25,13 @@ if ($music){
     $genre = $_POST['musicgenre'];
     $favorit = isset($_POST['musicfavorit']);
     
-    //falls der Interpret noch nicht existiert
-    $sql="SELECT iid,name FROM m_interpreter WHERE name = $interpreter";
+    //falls das Lied schon eingetragen ist wird kein neuer Eintrag erstellt
+    $sql="SELECT interpreter,albumtitel,erscheinungsjahr,genre FROM music WHERE  interpreter=$interpreter AND albumtitel=$albumtitel AND erscheinungsjahr=$jahr";
     $result = $mysqli->query($sql);
     if ($result->num_rows < 1) {
-        $sqlInsert = "INSERT INTO m_interpreter (name) VALUES ($interpreter)";
+        $sqlInsert = "INSERT INTO music (interpreter,albumtitel,erscheinungsjahr,songs,genre,favorit) VALUES ($interpreter,$albumtitel,$jahr,$songs,$genre,$favorit)";
         $result = $mysqli->query($sqlInsert);
-        $result = $mysqli->query($sql);
     }
-    $row = $result->fetch_assoc();
-    $iid = row["iid"];
-    
-    //falls das Album noch nicht existiert
-    $sql="SELECT aid,titel FROM m_albumtitel WHERE titel = $albumtitel";
-    $result = $mysqli->query($sql);
-    if ($result->num_rows < 1) {
-        $sqlInsert = "INSERT INTO m_albumtitel (titel,songs,jahr,favorit,genre) VALUES ($albumtitel,$songs,$jahr,$favorit,$genre)";
-        $result = $mysqli->query($sqlInsert);
-        $result = $mysqli->query($sql);
-    }
-    $row = $result->fetch_assoc();
-    $aid = row["aid"];
-    
-    //falls die beziehung zwischen Interpreter und Album noch nicht existiert
-    $sql = "SELECT iid,aid FROM m_interpreter_albumtitel WHERE iid = $iid AND aid = $aid";
-    $result = $mysqli->query($sql);
-    if ($result->num_rows < 1)  {
-        $sqlInsert = "INSERT INTO m_interpreter_albumtitel (iid,aid) VALUES ($iid,$aid)";
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
 } else {
     $filmtitel = $_POST['filmtitel'];
     $regie = $_POST['regie'];
@@ -71,11 +40,13 @@ if ($music){
     $schauspieler = $_POST['schauspieler'];
     $filmgenre = $_POST['filmgenre'];
     $filmfavorit = isset($_POST(['filmfavorit']));
-}
 
-foreach($_GET as $value) {
-	$txt = $txt."$value,";
+    //falls der Film schon eingetragen ist wird kein neuer Eintrag erstellt
+    $sql="SELECT filmtitel,regie,drehbuch,erscheinungsjahr,genre FROM film WHERE filmtitel=$filmtitel AND regie=$regie AND drehbuch=$drehbuch AND erscheinungsjahr=$erscheinungsjahr AND genre=$filmgenre";
+    $result = $mysqli->query($sql);
+    if ($result->num_rows < 1) {
+        $sqlInsert="INSERT INTO film (filmtitel,regie,drehbuch,erscheinungsjahr,schauspieler,genre,favorit) VALUES ($filmtitel,$regie,$drehbuch,$erscheinungsjahr,$schauspiler,$filmgenre,$filmfavorit)";
+        $result = $mysqli->query($sqlInsert);
+    }
 }
-fwrite($handle, substr($txt, 0, -1)."\n");
-fclose($handle);
 ?>
